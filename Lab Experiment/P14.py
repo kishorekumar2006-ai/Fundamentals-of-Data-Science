00cdc41ec10928bbc98b36b6b3b4290242b9d321
kishorekumar2006-ai <pkishorekumar5257@gmail.com>
2026-08-30 20:16:11 +0530

    Delete Lab Experiment directory

--- Lab Experiment/P14.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("customer_data.csv")
-
-age_frequency = df["Age"].value_counts().sort_index()
-
-print("Age Frequency Distribution:")
-print(age_frequency)
--- Lab Experiment/P15.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("likes_data.csv")
-
-likes_frequency = df["Likes"].value_counts().sort_index()
-
-print("Likes Frequency Distribution:")
-print(likes_frequency)
